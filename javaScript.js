eb3a82cbc28d834c44648fccef825ad1119d1d12
Yehya Ahmed <yayahmed@amazon.com>
2022-09-21 18:29:18 -0400

    Added a button that changes background color using RNG

--- javaScript.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const x = 5;
-console.log(x);
--- scripts/javaScript.js
+++ scripts/javaScript.js
@@ -1,2 +1,12 @@
 "use strict";
 
+const button = document.getElementById('button');
+
+button.addEventListener('click', onClick);
+
+function onClick() {
+    console.log(5);
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor);
+     document.body.style.backgroundColor = "#"+randomColor;
+};
